9d14e310c41f7230e28c9277f48c52d3acbb8133
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-09-27 03:13:53 +0100

    frontend connect wallet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// const client_id = import.meta.env.VITE_APP_CLIENT_ID
+
+import {
+  ConnectButton, useWallet,
+} from '@suiet/wallet-kit';
+
+const App = () => {
+  const wallet = useWallet();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <header>
+        <ConnectButton>Connect Wallet</ConnectButton>
+      </header>
+      <h3>
+        Welcome to Game 5 Casino!
+      </h3>
+      <p>
+
+        <span className="gradient">Current chain of wallet: </span>
+        {wallet.chain.name}
       </p>
+
     </>
   )
-}
+};
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+import { WalletProvider } from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
+
+// take react@18 project as an example
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <WalletProvider>
     <App />
-  </React.StrictMode>,
-)
+  </WalletProvider>
+);
