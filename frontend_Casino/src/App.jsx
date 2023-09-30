6f708aa3577f79753eb5ba381aefa00156e32e34
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-09-30 20:46:40 +0100

    casino-frontend

--- frontend_Casino/src/App.jsx
+++ frontend_Casino/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 
 import {
-    ConnectButton, useWallet,  
-    useAccountBalance,
-    SuiChainId,
-    ErrorCode,
-    formatSUI
-  } from '@suiet/wallet-kit';
+  ConnectButton, useWallet,
+  useAccountBalance,
+  SuiChainId,
+  ErrorCode,
+  formatSUI
+} from '@suiet/wallet-kit';
 import '@suiet/wallet-kit/style.css';
-import {TransactionBlock} from '@mysten/sui.js/transactions'
-import {useMemo} from "react";
+import { TransactionBlock } from '@mysten/sui.js/transactions'
+import { useMemo } from "react";
 
 
 
@@ -23,105 +23,109 @@ const App = () => {
 
 
   const wallet = useWallet();
-  let arrayAccountObjects  =[];
-  const {balance} = useAccountBalance();
+  let arrayAccountObjects = [];
+  const { balance } = useAccountBalance();
 
   const casinoContractAddr = useMemo(() => {
     if (!wallet.chain) return '';
     return Casino.get(wallet.chain.id) ?? '';
   }, [wallet]);
+  // try to get casino balance
+  const casinoBalance = useMemo(() => {
+    if (!wallet.chain) return '';
+    return Casino.get(wallet.balance) ?? '';
+  }, [wallet]);
+  console.log('casinoContractAddr', casinoBalance);
 
-console.log('casinoContractAddr', casinoContractAddr);
-
-const casinoAddress="0x5835e6527d7362c7665bf98bf9b18c550e634a815d064eef791bbbadb6c4d8cc";
-console.log('casinoAddress', casinoAddress);
-
-
-
-function uint8arrayToHex(value ) {
-  if (!value) return ''
-   return value.toString('hex')
-}
+  const casinoAddress = "0x5835e6527d7362c7665bf98bf9b18c550e634a815d064eef791bbbadb6c4d8cc";
+  console.log('casinoAddress', casinoAddress);
 
 
-async function handleExecuteMoveCall(target) {
-  if (!target) return;
 
-  try {
-    const tx = new TransactionBlock()
-    tx.moveCall({
-      target: target ,
-      arguments: [
-        tx.pure('Suiet NFT'),
-        tx.pure('Suiet Sample NFT'),
-        tx.pure('https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4')
-      ]
-    })
-    const resData = await wallet.signAndExecuteTransactionBlock({
-      transactionBlock: tx,
-    });
-    console.log('executeMoveCall success', resData);
-    alert('executeMoveCall succeeded (see response in the console)');
-  } catch (e) {
-    console.error('executeMoveCall failed', e);
-    alert('executeMoveCall failed (see response in the console)');
+  function uint8arrayToHex(value) {
+    if (!value) return ''
+    return value.toString('hex')
   }
-}
 
 
-async function handleSignMsg() {
-  if (!wallet.account) return
-  try {
-    const msg = 'Hello world!'
-    const msgBytes = new TextEncoder().encode(msg)
-    const result = await wallet.signMessage({
-      message: msgBytes
-    })
-    const verifyResult = await wallet.verifySignedMessage(result, wallet.account.publicKey)
-    console.log('verify signedMessage', verifyResult)
-    if (!verifyResult) {
-      alert(`signMessage succeed, but verify signedMessage failed`)
-    } else {
-      alert(`signMessage succeed, and verify signedMessage succeed!`)
-    }
-  } catch (e) {
-    console.error('signMessage failed', e)
-    alert('signMessage failed (see response in the console)')
-  }
-}
-
-
-
-
-async function DeposittoCasino() {
-  
-  console.log('DeposittoCasino 0', wallet)
-  if (!wallet.account) return
-  console.log('DeposittoCasino 1', wallet.getAccounts())
-  const target=casinoContractAddr+"::G5Game_core::depositToCasino";
+  async function handleExecuteMoveCall(target) {
+    if (!target) return;
 
     try {
-    const tx = new TransactionBlock()
-    const coin = tx.splitCoins(tx.gas, [tx.pure(500000000)]);
-    tx.setGasBudget(100000000);
-       tx.moveCall({
-         target: target,
-         arguments: [
+      const tx = new TransactionBlock()
+      tx.moveCall({
+        target: target,
+        arguments: [
+          tx.pure('Suiet NFT'),
+          tx.pure('Suiet Sample NFT'),
+          tx.pure('https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4')
+        ]
+      })
+      const resData = await wallet.signAndExecuteTransactionBlock({
+        transactionBlock: tx,
+      });
+      console.log('executeMoveCall success', resData);
+      alert('executeMoveCall succeeded (see response in the console)');
+    } catch (e) {
+      console.error('executeMoveCall failed', e);
+      alert('executeMoveCall failed (see response in the console)');
+    }
+  }
+
+
+  async function handleSignMsg() {
+    if (!wallet.account) return
+    try {
+      const msg = 'Hello world!'
+      const msgBytes = new TextEncoder().encode(msg)
+      const result = await wallet.signMessage({
+        message: msgBytes
+      })
+      const verifyResult = await wallet.verifySignedMessage(result, wallet.account.publicKey)
+      console.log('verify signedMessage', verifyResult)
+      if (!verifyResult) {
+        alert(`signMessage succeed, but verify signedMessage failed`)
+      } else {
+        alert(`signMessage succeed, and verify signedMessage succeed!`)
+      }
+    } catch (e) {
+      console.error('signMessage failed', e)
+      alert('signMessage failed (see response in the console)')
+    }
+  }
+
+
+
+
+  async function DeposittoCasino() {
+
+    console.log('DeposittoCasino 0', wallet)
+    if (!wallet.account) return
+    console.log('DeposittoCasino 1', wallet.getAccounts())
+    const target = casinoContractAddr + "::G5Game_core::depositToCasino";
+
+    try {
+      const tx = new TransactionBlock()
+      const coin = tx.splitCoins(tx.gas, [tx.pure(500000000)]);
+      tx.setGasBudget(100000000);
+      tx.moveCall({
+        target: target,
+        arguments: [
           tx.object(casinoAddress),
           coin
         ]
       });
-      
+
 
 
       const resData = await wallet.signAndExecuteTransactionBlock({
         transactionBlock: tx,
       });
       console.log('executeMoveCall success', resData);
-    //  alert('executeMoveCall succeeded (see response in the console)');
+      //  alert('executeMoveCall succeeded (see response in the console)');
     } catch (e) {
       console.error('executeMoveCall failed', e);
-    //  alert('executeMoveCall failed (see response in the console)');
+      //  alert('executeMoveCall failed (see response in the console)');
     }
   }
 
@@ -132,11 +136,11 @@ async function DeposittoCasino() {
   function getAddress() {
     if (!wallet.account) return null;
     return wallet.account.address;
-  }    
+  }
 
 
   // gets the user's object and checks if we have a casino ownership.
-    // We also keep a list of SUI Coin addresses to use for transactions.
+  // We also keep a list of SUI Coin addresses to use for transactions.
   //   const getUserCasinoOwnershipAndUserCoinAddresses = () => {
   //     const address = getAddress();
   //     if(!address) return;
@@ -173,7 +177,7 @@ async function DeposittoCasino() {
 
 
 
-const chainName = (chainId ) => {
+  const chainName = (chainId) => {
     switch (chainId) {
       case SuiChainId.MAIN_NET:
         return 'Mainnet'
@@ -196,40 +200,40 @@ const chainName = (chainId ) => {
     console.log('callGamblefct 0', wallet)
     if (!wallet.account) return
     console.log('callGamblefct 1', wallet.getAccounts())
-    const target="0xfa5f05f0915dca5757ca7e499e9fa39e61ed354b0fcdee370aa25072e687ccd5::G5Game_core::anybodydoNothing";
-  
-      try {
-        const tx = new TransactionBlock()
-       tx.setGasBudget(100000000);
+    const target = "0xfa5f05f0915dca5757ca7e499e9fa39e61ed354b0fcdee370aa25072e687ccd5::G5Game_core::anybodydoNothing";
+
+    try {
+      const tx = new TransactionBlock()
+      tx.setGasBudget(100000000);
       const coin = tx.splitCoins(tx.gas, [tx.pure(50000)]);
-         tx.moveCall({
-           target: target,
-           arguments: [
-         //   tx.pure( OwnerCap),
-            //tx.pure("0xf0758770c3a1439e873975f5add81d215a3914fba6869b84965b4255bfaa6740"),
-            coin
-            //tx.pure("500000"),
+      tx.moveCall({
+        target: target,
+        arguments: [
+          //   tx.pure( OwnerCap),
+          //tx.pure("0xf0758770c3a1439e873975f5add81d215a3914fba6869b84965b4255bfaa6740"),
+          coin
+          //tx.pure("500000"),
           //  tx.pure("0x23b130490999dee73eac63410117b80c227caf819f897911477b43411460fb9a"),
-            
-          ]
-        });
-        
-        
-  
-  
-        const resData = await wallet.signAndExecuteTransactionBlock({
-          transactionBlock: tx,
-        });
-        console.log('executeMoveCall success', resData);
-     //   alert('executeMoveCall succeeded (see response in the console)');
-      } catch (e) {
-        console.error('executeMoveCall failed', e);
-     //   alert('executeMoveCall failed (see response in the console)');
-      }
+
+        ]
+      });
+
+
+
+
+      const resData = await wallet.signAndExecuteTransactionBlock({
+        transactionBlock: tx,
+      });
+      console.log('executeMoveCall success', resData);
+      //   alert('executeMoveCall succeeded (see response in the console)');
+    } catch (e) {
+      console.error('executeMoveCall failed', e);
+      //   alert('executeMoveCall failed (see response in the console)');
     }
-  
-  
-  
+  }
+
+
+
 
 
 
@@ -248,63 +252,58 @@ const chainName = (chainId ) => {
   return (
     <>
       <header>
+
+        <h1>
+          Welcome to Group 5 Casino! 🎉
+        </h1>
       </header>
-      <h3>
-        Welcome to Game 5 Casino! 🎉
-      </h3>
       <div className="card">
-      <ConnectButton
-          onConnectError={(error) => {
-            if (error.code === ErrorCode.WALLET__CONNECT_ERROR__USER_REJECTED) {
-              console.warn('user rejected the connection to ' + error.details?.wallet)
-            } else {
-              console.warn('unknown connect error: ', error)
-            }
-          }}
-        />
-     
+        <div className='connect-btn'>
+          <ConnectButton
+            onConnectError={(error) => {
+              if (error.code === ErrorCode.WALLET__CONNECT_ERROR__USER_REJECTED) {
+                console.warn('user rejected the connection to ' + error.details?.wallet)
+              } else {
+                console.warn('unknown connect error: ', error)
+              }
+            }}
+          >Connect your wallet</ConnectButton></div>
+
 
         {!wallet.connected ? (
-          <p>Connect DApp with Suiet wallet from now!</p>
+          <p>Connect DApp with Sui wallet!</p>
         ) : (
-          <div>
-            <div>
-              <p>current wallet: {wallet.adapter?.name}</p>
-              <p>
-                wallet status:{' '}
+          <div className='container'>
+            <div className="wallet-div">
+              <p className="info">Current wallet: {wallet.adapter?.name}</p>
+              <p className="info">
+                Wallet status:{' '}
                 {wallet.connecting
                   ? 'connecting'
                   : wallet.connected
                     ? 'connected'
                     : 'disconnected'}
               </p>
-              <p>wallet address: {wallet.account?.address}</p>
-              <p>current network: {wallet.chain?.name}</p>
-              <p>wallet balance: {formatSUI(balance ?? 0, {
+              <p className="wallet-address">Wallet address: {wallet.account?.address}</p>
+              <p className="info">Current network: {wallet.chain?.name}</p>
+              <p className="info">Wallet balance: {formatSUI(balance ?? 0, {
                 withAbbr: false
               })} SUI</p>
-              <p>wallet publicKey: {uint8arrayToHex(wallet.account?.publicKey)}</p>
-              <p>casino address: {casinoContractAddr } </p>
-              <p> list of objects: {arrayAccountObjects}</p>
-
             </div>
-            <div className={'btn-group'} style={{margin: '8px 0'}}>
-              {casinoContractAddr && (
-                <button onClick={() => handleExecuteMoveCall(casinoContractAddr)}>Call {chainName(wallet.chain?.id)} fct</button>
-              )}
-              <button onClick={handleSignMsg}>signMessage</button>
-              <button onClick={DeposittoCasino}>Deposit To Casino </button>
-              <button onClick={callGamblefct}>Call Gamble</button>
+            <div className='casino-div'>
+              <h3>Deposit some SUI to the casino and call gamble</h3>
+              <p className="wallet-address">Casino address: {casinoContractAddr} </p>
+              <p className="info">Casino balance: {formatSUI(casinoBalance ?? 0, {
+                withAbbr: false
+              })} SUI</p>
+            </div>
+            <div className='btn-group'>
+              <button className="action-btn" onClick={DeposittoCasino}>Deposit To Casino</button>
+              <button className="action-btn" onClick={callGamblefct}>Call Gamble</button>
             </div>
           </div>
         )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and Suiet logos to learn more
-      </p>
-
-
-
 
     </>
   )
